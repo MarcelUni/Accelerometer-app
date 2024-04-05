ecabe1997ef0d91819b4d8787bb925077cf276f3
Marcelkress <Marcel@kress.dk>
2024-04-05 08:05:50 +0200

    Works

--- Assets/Scripts/SaveAccelerometerData.cs
+++ Assets/Scripts/SaveAccelerometerData.cs
@@ -18,7 +18,7 @@ public class SaveAccelerometerData : MonoBehaviour
     public float recordTime = 5f;   // How long it should record data
     private float elapsedTime = 0f;
 
-    // XML stuff
+    // JSON stuff
     private string datapath;    // Path to save data
     private string datafile;    // File to save data
 
@@ -36,7 +36,7 @@ public class SaveAccelerometerData : MonoBehaviour
 
     void Start()
     {
-        NewDirectory();
+        NewDirectory();             //Create a new directory
     }
 
     public void StartRecording()    //Button to start recording
@@ -47,16 +47,22 @@ public class SaveAccelerometerData : MonoBehaviour
 
     private IEnumerator RecordData()    //Record data for a certain time
     {
-        while (elapsedTime < recordTime)
+        Console.text += "\nRecording for " + recordTime + " seconds";
+
+        while (elapsedTime < recordTime)// Record data for a certain time
         {
             elapsedTime += .5f;
             WriteToList();              // Add data to list
-            Console.text += "\n" + elapsedTime + " seconds";
             yield return new WaitForSeconds(.5f);
         }
         if(elapsedTime >= recordTime)
         {
             elapsedTime = 0f;
+            Console.text += "\nData points:";
+            foreach (Vector3 v in data) // Show the data points
+            {
+                Console.text += "\n" + v;
+            }
             SaveData();                 //When time is up, save the data
         }
     }
@@ -65,7 +71,6 @@ public class SaveAccelerometerData : MonoBehaviour
     {
         Vector3 currentData = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
         data.Add(currentData); 
-        Debug.Log(currentData);
     }
 
     private void NewDirectory()        //Create a new directory if it doesn't exist
@@ -79,13 +84,14 @@ public class SaveAccelerometerData : MonoBehaviour
     private void SaveData()           //Save the data to a json file
     {
         Console.text += "\nRecording finished and saving Data";
-        ListOfData dataToSave = new ListOfData();
 
-        dataToSave.AccelerometerData = data;
+        ListOfData dataToSave = new ListOfData();   //Create a in instance of the Listofdata class
 
-        string jsonString = JsonUtility.ToJson(dataToSave, true);
+        dataToSave.AccelerometerData = data;        // Add the data in the list to the instance
 
-        using(StreamWriter stream = File.CreateText(datafile))
+        string jsonString = JsonUtility.ToJson(dataToSave, true);   //Convert the instance to a json string
+
+        using(StreamWriter stream = File.CreateText(datafile))      //Write the json string to a file
         {
             stream.WriteLine(jsonString);
         }
